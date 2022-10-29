e02e01245851f17220199e0a1b0836a51f2426da
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2022-10-29 12:03:49 +0100

    trying so hard to fix error

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var button = document.querySelector("#app-mount > div.appDevToolsWrapper-1QxdQf > div > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div.container-1eFtFS > div > div > div > section > div.container-YkUktl > div.flex-2S1XBF.flex-3BkGQD.horizontal-112GEH.horizontal-1Piu5-.flex-3BkGQD.directionRow-2Iu2A9.justifyStart-2Mwniq.alignStretch-Uwowzr.noWrap-hBpHBz > button:nth-child(3)")
 console.log(button)
 
-button.addEventListener("click", function() {
+button.addEventListener("click", displayMenu);
+
+function displayMenu() {
     alert("TEST");
-});
+}
