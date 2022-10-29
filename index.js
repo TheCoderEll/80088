34f44a627457ae9c02f95150b640d8218bde1dc0
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2022-10-29 11:44:00 +0100

    click settings test

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-alert("test")
+var button = document.querySelector("#app-mount > div.appDevToolsWrapper-1QxdQf > div > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div.container-1eFtFS > div > div > div > section > div.container-YkUktl > div.flex-2S1XBF.flex-3BkGQD.horizontal-112GEH.horizontal-1Piu5-.flex-3BkGQD.directionRow-2Iu2A9.justifyStart-2Mwniq.alignStretch-Uwowzr.noWrap-hBpHBz > button:nth-child(3)")
+button.addEventListener("click", function() {
+    alert("TEST");
+});
